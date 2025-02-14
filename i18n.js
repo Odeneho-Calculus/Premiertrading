20ed11f3dc79ba1cccad73deff3ed17b7b86f0b3
Odeneho-Calculus <calculus069@gmail.com>
2025-02-14 22:45:42 +0000

    fix deployment errors

--- i18n.js
+++ i18n.js
@@ -1,14 +1,15 @@
 import { getRequestConfig } from 'next-intl/server';
 import { notFound } from 'next/navigation';
 import { locales } from './config';
+import { headers } from 'next/headers';
 
-export default getRequestConfig(async ({ locale }) => {
-  // Validate that the incoming locale is supported
+export default getRequestConfig(async () => {
+  const locale = headers().get('x-next-intl-locale') || 'en';
+
   if (!locales.includes(locale)) {
     notFound();
   }
 
-  // Load messages for the requested locale
   return {
     messages: (await import(`./messages/${locale}.json`)).default
   };
